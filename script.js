4d4a8c5284d152d7cc19f20c950fb5c1af871237
nataliecali <55113277+nataliecali@users.noreply.github.com>
2021-03-18 15:36:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var mugen = new MUGEN();
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", actionRadius: 100, timeBetweenNPCs: 4000, npcCollisionRadius: 100, actionDuration:600, jumpHeight: 300, makeResponsive:true});
+});
